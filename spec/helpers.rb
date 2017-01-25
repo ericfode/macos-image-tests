2e1ea7ff04256f20c1cb8ff993a81dc6045bfa58
Marc O'Morain <github.com@marcomorain.com>
2017-01-25 13:43:18 +0000

    Add simulators key

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -5,11 +5,8 @@ module Helpers
   # Load the simulators for the given xcode version.
   # Return an array that matched the JSON output format.
   def simulators(version)
-    x = YAML::load(File.read("spec/fixtures/xcode/#{version}"))
-    
-    puts(x)
-    
-    x.map do |name|
+    xcode = YAML::load(File.read("spec/fixtures/xcode/#{version}"))
+    xcode['simulators'].map do |name|
       { name: name }
     end.unshift({}) # Add an empty slot at the front to handle the `distiller` simulator.
   end
