1c006e68bdae62116a8d4e492320985a8ec8212d
Marc O'Morain <github.com@marcomorain.com>
2017-01-25 16:05:05 +0000

    Remove values from schema, move to ruby spec

--- spec/software_spec.rb
+++ spec/software_spec.rb
@@ -26,6 +26,10 @@ describe 'software' do
         python: "Python 2.7.10",
         xctool: "0.2.9"
       },
+      ruby: {
+        version: "ruby 2.3.1p112 (2016-04-26 revision 54768) [x86_64-darwin15]"
+      }
+       
     )
   end
 
